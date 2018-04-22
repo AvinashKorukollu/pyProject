4ddfeacb845c83d139d3fc36111b839cfb8dab51
Avinash <avinash.korukollu@gmail.com>
2018-04-22 18:28:27 -0400

    Move everything to main class

--- main.py
+++ main.py
@@ -2,8 +2,60 @@ import sys, termios, tty, os, time
 import RPi.GPIO as gpio
 from .move import Move
 
-trigger_pin = 18
-echo_pin = 25
+gpio.setmode(gpio.BCM)
+
+class Move:
+    def __init__(self):
+    	gpio.setup(23, gpio.OUT)
+        gpio.setup(24, gpio.OUT)
+        gpio.setup(17, gpio.OUT)
+        gpio.setup(22, gpio.OUT)
+        gpio.setup(26, gpio.OUT)
+        gpio.setup(6, gpio.OUT)
+        gpio.setup(5, gpio.OUT)
+        gpio.setup(16, gpio.OUT)
+
+    def move_forward(self):
+        gpio.output(23, True)
+        gpio.output(24, False)
+        gpio.output(17, True)
+        gpio.output(22, False)
+        gpio.output(26, True)
+        gpio.output(6, False)
+        gpio.output(5, True)
+        gpio.output(16, False)
+
+    def move_backward(self):
+        gpio.output(23, False)
+        gpio.output(24, True)
+        gpio.output(17, False)
+        gpio.output(22, True)
+        gpio.output(26, False)
+        gpio.output(6, True)
+        gpio.output(5, False)
+        gpio.output(16, True)
+
+    def move_right(self):
+        gpio.output(23, True)
+        gpio.output(24, False)
+        gpio.output(17, True)
+        gpio.output(22, False)
+        gpio.output(26, False)
+        gpio.output(6, False)
+        gpio.output(5, False)
+        gpio.output(16, False)
+
+    def move_left(self):
+        gpio.output(23, False)
+        gpio.output(24, False)
+        gpio.output(17, False)
+        gpio.output(22, False)
+        gpio.output(26, False)
+        gpio.output(6, True)
+        gpio.output(5, False)
+        gpio.output(16, True)
+
+
 move = Move()
 
 
@@ -21,7 +73,7 @@ def getch():
 
 button_delay = 0.2
 
-if __name__ == '__main__':
+try:
     char = getch()
     if char == "p":
         exit(0)
@@ -35,3 +87,10 @@ if __name__ == '__main__':
         move.move_backward()
     else:
         time.sleep(button_delay)
+
+except KeyboardInterrupt:
+	print('KeyboardInterrupt')
+
+finally:
+	print('gpio cleanup')
+	gpio.cleanup()
